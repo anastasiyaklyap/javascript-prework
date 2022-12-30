c30e9d3d1c1cc2b1ab6d220541f0a305516a4995
anastasiyaklyap <anastasiya.klyap@gmail.com>
2022-12-30 15:03:01 +0100

    add logic for result of the game

--- js/script.js
+++ js/script.js
@@ -1,25 +1,49 @@
-const randomNumber = Math.floor(Math.random() * 3 + 1);
-console.log(`wylosowana liczba to ${randomNumber}`);
-const computerMove =
-	randomNumber === 1 ? 'kamień' : randomNumber === 2 ? 'papier' : 'nożyce';
+const getMoveName = ({ argMoveId }) => {
+	console.log(`wywołano funkcję getMoveName z argumentem: ${argMoveId}`);
+	switch (argMoveId) {
+		case '1':
+			return 'kamień';
+			break;
+		case '2':
+			return 'papier';
+			break;
+		case '3':
+			return 'nożyce';
+			break;
+		default:
+			printMessage(
+				`Nie znam ruchu o id ${argMoveId}. Zakładam, że chodziło o "kamień".`
+			);
+			return 'kamień';
+	}
+};
 
-printMessage(`Mój ruch: ${computerMove}`);
+const displayResult = ({ argPlayerMove, argComputerMove }) => {
+	console.log(
+		`wywołano funkcję displayResults z argumentami: ${argPlayerMove}, ${argComputerMove}`
+	);
 
-let playerMove;
+	if (argPlayerMove == 'papier' && argComputerMove == 'kamień') {
+		printMessage('Wygrywasz!');
+	} else if (argPlayerMove == 'kamień' && argComputerMove == 'nożyce') {
+		printMessage('Wygrywasz!');
+	} else if (argPlayerMove == 'nożyce' && argComputerMove == 'papier') {
+		printMessage('Wygrywasz!');
+	} else if (argPlayerMove === argComputerMove) {
+		printMessage('Remis');
+	} else {
+		printMessage('Przegrywasz :(');
+	}
+	printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+};
 const playerInput = prompt(
 	'Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.'
 );
-console.log(`Wpisana odpowiedź to: ${playerInput}`);
-
-if (playerInput == 1) {
-	playerMove = 'kamień';
-} else if (playerInput == 2) {
-	playerMove = 'papier';
-} else if (playerInput == 3) {
-	playerMove = 'nożyce';
-} else {
-	printMessage('błędny ruch');
-	playerMove = 'kamień';
-}
-
-printMessage(`Twój ruch ${playerMove}`);
+console.log(`wybór ruchu gracza to: ${playerInput}`);
+const playerMove = getMoveName({ argMoveId: playerInput });
+console.log(`ruch gracza to: ${playerMove}`);
+const randomNumber = Math.floor(Math.random() * 3 + 1).toString();
+console.log(`wylosowana liczba to: ${randomNumber}`);
+const computerMove = getMoveName({ argMoveId: randomNumber });
+console.log(`ruch komputera to: ${computerMove}`);
+displayResult({ argPlayerMove: playerMove, argComputerMove: computerMove });
