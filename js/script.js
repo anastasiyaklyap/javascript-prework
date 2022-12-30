8ee3c230777b8130321de667727f41342856642d
anastasiyaklyap <anastasiya.klyap@gmail.com>
2022-12-30 13:35:31 +0100

    add init logic

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-const userMove = 'papier';
-const computerMove = 'kamień';
-printMessage(
-	`Zagrałem ${computerMove}! Jeśli Twój ruch to ${userMove} to wygrywasz!`
-);
+const randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log(`wylosowana liczba to ${randomNumber}`);
+const computerMove =
+	randomNumber == 1 ? 'kamień' : randomNumber == 2 ? 'papier' : 'nożyce';
+
+printMessage(`Mój ruch: ${computerMove}`);
